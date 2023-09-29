5b9d81ad49247c408a5e8bfbd24b75dd7b849ec1
ToastedGMS <gabrielsiqueira3052@gmail.com>
2023-09-29 15:43:22 -0300

    moved cell creation to DOM

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const gameboard = (() =>{
+
+    const board = ['', '', '', '', '', '', '', '', ''];
     
-    const cells = document.querySelectorAll('[data-type="cell"]')
-    cells.forEach(element => {
-        element.addEventListener('click', function(){
-            if (element.style.backgroundColor === '') {
-            element.style.backgroundColor = 'black';
-        } else if (element.style.backgroundColor === 'black') {
-            element.style.backgroundColor = '';
-        }})
-    });
+    const gameBoard = document.querySelector('[data-type="gameboard"]')
+    board.forEach(element => {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        cell.setAttribute('data-type', 'cell');
+        cell.innerHTML = element;
+        gameBoard.appendChild(cell);
+    })
+    
+    const cell = document.querySelectorAll('[data-type="cell"]');
+    return {cell};
 })()
+
