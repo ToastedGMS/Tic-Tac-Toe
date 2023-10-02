e6b4c47da282f296d1663bca0d6710254aa86f5a
Gabriel Siqueira <125387064+ToastedGMS@users.noreply.github.com>
2023-10-01 23:42:18 -0300

    implementing winner check function

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 const gameboard = (() =>{
 
     const board = ['', '', '', '', '', '', '', '', ''];
+    let nextTurn = 'X';
     
     const gameBoard = document.querySelector('[data-type="gameboard"]')
-    board.forEach(element => {
+    board.forEach((element, index) => {
         const cell = document.createElement('div');
         cell.classList.add('grid-cell');
         cell.setAttribute('data-type', 'cell');
+        cell.setAttribute('data-index', index)
         cell.innerHTML = element;
         gameBoard.appendChild(cell);
-    })
     
-    const cell = document.querySelectorAll('[data-type="cell"]');
-    return {cell};
-})()
-
-const game = (() => {
-    let nextTurn = 'X';
-    
-    gameboard.cell.forEach(element => {
+    document.querySelectorAll('[data-type="cell"]').forEach(element => {
         element.addEventListener('click', function(){
             function changeTurn(){
                 if (nextTurn === 'X'){
@@ -30,8 +24,34 @@ const game = (() => {
 
             if (element.innerHTML === ''){
                 element.innerHTML = nextTurn;
+                board[index] = nextTurn;
                 changeTurn();
             };
         })
     })
+})
+return {board};
+})()
+
+function checkWinner(){
+    let a = gameboard.board.slice(0,3)
+    let b = gameboard.board.slice(3,6)
+    let c = gameboard.board.slice(6)
+    let d = [];
+    let e = [];
+    d.push(gameboard.board[0], gameboard.board[4], gameboard.board[8]);
+    e.push(gameboard.board[2], gameboard.board[4], gameboard.board[6]);
+
+    return {a,b,c,d,e};
+};
+
+function checkArr(arr, string){
+    if (arr[0] === string &&
+        arr[1] === string &&
+        arr[2] === string){
+            return `${string} wins`
+        } else return 'nigger'
+};
+
+const game =(() => {
 })()
