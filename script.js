be49b0766156201aa643487df501863109eea474
ToastedGMS <gabrielsiqueira3052@gmail.com>
2023-10-03 14:43:20 -0300

    added function that checks for and returns winner

--- script.js
+++ script.js
@@ -34,24 +34,42 @@ return {board};
 })()
 
 function checkWinner(){
-    let a = gameboard.board.slice(0,3)
-    let b = gameboard.board.slice(3,6)
-    let c = gameboard.board.slice(6)
-    let d = [];
-    let e = [];
-    d.push(gameboard.board[0], gameboard.board[4], gameboard.board[8]);
-    e.push(gameboard.board[2], gameboard.board[4], gameboard.board[6]);
+    
+    function arrFactory(a, b, c){
+        let arr = [];
+        arr.push(a, b, c);
+        return arr;
+    };
 
-    return {a,b,c,d,e};
+    let board = gameboard.board;
+
+    let a = arrFactory(board[0], board[1], board[2]);
+    let b = arrFactory(board[3], board[4], board[5]);
+    let c = arrFactory(board[6], board[7], board[8]);
+    let d = arrFactory(board[0], board[3], board[6]);
+    let e = arrFactory(board[1], board[4], board[7]);
+    let f = arrFactory(board[2], board[5], board[8]);
+    let g = arrFactory(board[0], board[4], board[8]);
+    let h = arrFactory(board[2], board[4], board[6]);
+    
+    function findWinner(arr){
+        if (arr[0] === arr[1] &&
+            arr[1] === arr[2]){
+               return `${arr[0]} wins`} 
+        else return 'no win detected'
+    };
+
+    let arr1 = [a,b,c,d,e,f,g,h];
+
+    const results = arr1.map(array => findWinner(array));
+    
+    function printWinner(){
+        if (results.includes('X wins')){
+            return 'X wins'
+        } else if (results.includes('O wins')){
+            return 'O wins'
+        } else return 'It\'s a tie'
+    };
+
+    return printWinner();
 };
-
-function checkArr(arr, string){
-    if (arr[0] === string &&
-        arr[1] === string &&
-        arr[2] === string){
-            return `${string} wins`
-        } else return 'nigger'
-};
-
-const game =(() => {
-})()
