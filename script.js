77b4e79e7776fa3d0864e8996af43bdb5be19937
Gabriel Siqueira <125387064+ToastedGMS@users.noreply.github.com>
2023-10-07 21:39:36 -0300

    function that alerts whether there was a winner, or a tie, do so before all cells are played

--- script.js
+++ script.js
@@ -33,43 +33,52 @@ const gameboard = (() =>{
 return {board};
 })()
 
-function checkWinner(){
-    
-    function arrFactory(a, b, c){
-        let arr = [];
-        arr.push(a, b, c);
-        return arr;
-    };
+const game = (() => {
+    let counter = 0;
+    document.querySelector('[data-type="gameboard"]').addEventListener('click', function(){
+        counter += 1;
+            
+        function arrFactory(a, b, c){
+                let arr = [];
+                arr.push(a, b, c);
+                return arr;
+        };
+        
+            let board = gameboard.board;
+        
+            let a = arrFactory(board[0], board[1], board[2]);
+            let b = arrFactory(board[3], board[4], board[5]);
+            let c = arrFactory(board[6], board[7], board[8]);
+            let d = arrFactory(board[0], board[3], board[6]);
+            let e = arrFactory(board[1], board[4], board[7]);
+            let f = arrFactory(board[2], board[5], board[8]);
+            let g = arrFactory(board[0], board[4], board[8]);
+            let h = arrFactory(board[2], board[4], board[6]);
+            
+        function findWinner(arr){
+            if (arr[0] === arr[1] &&
+                arr[1] === arr[2]){
+                    return `${arr[0]} wins`} 
+            else return 'no win detected'
+        };
+        
+            let arr1 = [a,b,c,d,e,f,g,h];
+        
+            const results = arr1.map(array => findWinner(array));
+            
+        function printWinner(){
+            if (results.includes('X wins')){
+                return 'X wins'
+            } else if (results.includes('O wins')){
+                return 'O wins'
+            } else return 'It\'s a tie'
+        };
+        
+        if (printWinner() === 'X wins' ||
+            printWinner() === 'O wins'){
+                alert(printWinner())}
+        else if (counter === 9){
+            alert(printWinner())}
+    })
 
-    let board = gameboard.board;
-
-    let a = arrFactory(board[0], board[1], board[2]);
-    let b = arrFactory(board[3], board[4], board[5]);
-    let c = arrFactory(board[6], board[7], board[8]);
-    let d = arrFactory(board[0], board[3], board[6]);
-    let e = arrFactory(board[1], board[4], board[7]);
-    let f = arrFactory(board[2], board[5], board[8]);
-    let g = arrFactory(board[0], board[4], board[8]);
-    let h = arrFactory(board[2], board[4], board[6]);
-    
-    function findWinner(arr){
-        if (arr[0] === arr[1] &&
-            arr[1] === arr[2]){
-               return `${arr[0]} wins`} 
-        else return 'no win detected'
-    };
-
-    let arr1 = [a,b,c,d,e,f,g,h];
-
-    const results = arr1.map(array => findWinner(array));
-    
-    function printWinner(){
-        if (results.includes('X wins')){
-            return 'X wins'
-        } else if (results.includes('O wins')){
-            return 'O wins'
-        } else return 'It\'s a tie'
-    };
-
-    return printWinner();
-};
+})();
